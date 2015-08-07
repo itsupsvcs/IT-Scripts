09dfa7beecca5be2ded2cb27c6b4be3d03fb6100
IT Support <it-support@tradingtechnologies.com>
2015-08-07 15:05:23 -0500

    Unix attributes fixed
    Unix attributes were not being applied because of the string literals

--- NewHire/NewHireSetup.ps1
+++ NewHire/NewHireSetup.ps1
@@ -483,13 +483,13 @@ $NIS = Get-ADObject "CN=int,CN=ypservers,CN=ypServ30,CN=RpcServices,CN=System,DC
 $maxUid = $NIS.msSFU30MaxUidNumber + 1
 Set-ADObject $NIS -Replace @{msSFU30MaxUidNumber = "$($maxUid)"}
 
- Set-ADUser -Identity "$($Username)" -Replace @{mssfu30nisdomain = "int"} #Enable NIS
-   Set-ADUser -Identity "$($Username)" -Replace @{gidnumber="10000"} #Set Group ID
+   Set-ADUser -Identity $Username -Replace @{mssfu30nisdomain = "int"} #Enable NIS
+   Set-ADUser -Identity $Username -Replace @{gidnumber="10000"} #Set Group ID
    $maxUid++ #Raise the User ID number
-   Set-ADUser -Identity "$($Username)" -Replace @{uidnumber=$maxUid} #Set User ID
-   Set-ADUser -Identity "$($Username)" -Replace @{loginshell="/bin/bash"} #Set user login shell
-   Set-ADUser -Identity "$($Username)" -Replace @{msSFU30Name="$($Username)"}
-   Set-ADUser -Identity "$($Username)" -Replace @{unixHomeDirectory="/home/$($Username)"}
+   Set-ADUser -Identity $Username -Replace @{uidnumber=$maxUid} #Set User ID
+   Set-ADUser -Identity $Username -Replace @{loginshell="/bin/bash"} #Set user login shell
+   Set-ADUser -Identity $Username -Replace @{msSFU30Name="$($Username)"}
+   Set-ADUser -Identity $Username -Replace @{unixHomeDirectory="/home/$($Username)"}
    Write-Host -Backgroundcolor Green -Foregroundcolor Black $usr.SamAccountName changed #Write Changed Username to console	
 
 #This is where we will Launch the GAM tool to add the user to Google
